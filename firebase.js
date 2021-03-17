cedadeab7ba55b7130cd1f60bcb694a60503f1d0
SofiyaOlenyn <olenynsofiya@gmail.com>
2021-03-17 20:06:28 +0200

    Init

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCh3OZXPKzQO2wYYA8ZZpRxEgPIY2FwkIo",
+    authDomain: "quiz-react-native-cw.firebaseapp.com",
+    projectId: "quiz-react-native-cw",
+    storageBucket: "quiz-react-native-cw.appspot.com",
+    messagingSenderId: "467895483224",
+    appId: "1:467895483224:web:f7efaf60c6ece48950c32d"
+};
