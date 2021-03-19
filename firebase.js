99d644c05aa71581eb1fcf708d78e0ab09574429
SofiyaOlenyn <olenynsofiya@gmail.com>
2021-03-19 19:08:52 +0200

    connection with DB + login

--- firebase.js
+++ firebase.js
@@ -1,3 +1,8 @@
+
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCh3OZXPKzQO2wYYA8ZZpRxEgPIY2FwkIo",
     authDomain: "quiz-react-native-cw.firebaseapp.com",
@@ -6,3 +11,15 @@ const firebaseConfig = {
     messagingSenderId: "467895483224",
     appId: "1:467895483224:web:f7efaf60c6ece48950c32d"
 };
+let app;
+
+if(firebase.apps.length === 0 ) {
+    app = firebase.initializeApp(firebaseConfig)
+}else {
+    app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export {db,auth};
---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+       <ScrollView>
+
+        </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+export default HomeScreen;
