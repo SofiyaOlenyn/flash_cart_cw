b2082d8568a7f10611465132540c344ec320e998
SofiyaOlenyn <olenynsofiya@gmail.com>
2021-05-13 23:18:29 +0300

    fix

--- screens/EditCardInMyScreen.js
+++ screens/EditCardInMyScreen.js
@@ -169,7 +169,7 @@ const EditCardInMyScreen = ({route, navigation}) => {
         {
             ID2=card.backImage
         }
-        if(imageUrlFront != "" && imgBackChanged ) {
+        if(imageUrlFront != "" && imgFrontChanged ) {
             const uuid1 = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c, r) => ('x' == c ? (r = Math.random() * 16 | 0) : (r & 0x3 | 0x8)).toString(16));
             ID1 = uuid1() + "-" + auth.currentUser.uid;
             await uploadImage(ID1,imageUrlFront)
